d0ebe3b2dbef077e1117121202f3528b06c70fde
kjurczyk <kjurczyk@ufl.edu>
2021-12-09 12:15:22 -0500

    plot images py started

---
+++ plot_10_images.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  9 12:11:16 2021
+
+@author: Katarina
+"""
+
+dirPathDistilled = "distilled_images/"
+dirPathAncestor = "ancestor_images/"
+
+# plot 10 images (1 from each class) for the distilled images we are using: 10_distilled_batch_size_64_5epochs.gz
+
+# plot 10 images (1 from each class) for the ancestor images we are using: first_1000_train_images.gz
